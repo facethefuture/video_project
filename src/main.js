b0891169d4e7d9e30f20a84577d3b1194734b5bd
chris <chris@jingdigital.com>
2019-01-28 23:46:31 +0800

    add proxy

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Mint from 'mint-ui';
+import Axios from 'axios'
+Vue.use(Mint);
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = Axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/views/video/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <ul
+        v-infinite-scroll="loadMore"
+        infinite-scroll-disabled="loading"
+        infinite-scroll-distance="10">
+        <li v-for="(item,index) in videoList" :key="index">{{item.id}}{{item.name}}</li>
+      </ul>
+      <!--<ul>-->
+        <!--<li v-for="(item,index) in videoList" :key="index">{{item.id}}{{item.name}}</li>-->
+      <!--</ul>-->
+    </div>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  name: "index",
+  data () {
+    return {
+      videoList: [],
+      loading: false
+    }
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    getList (){
+      this.$axios.get("api/web01/queryStudent").then((res) => {
+        console.log(res)
+        res.data.forEach((item) => {
+          this.videoList.push(item)
+        })
+      })
+    },
+    loadMore () {
+      this.getList()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
