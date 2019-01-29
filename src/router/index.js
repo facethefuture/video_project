7732bbcee6858c25c6a983cf0cfd8cd338584cd8
chris <chris@jingdigital.com>
2019-01-29 19:55:59 +0800

    saved

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import videoPage from '@/views/video/index.vue'
+import video from './video'
 
 Vue.use(Router)
 
@@ -9,7 +9,9 @@ export default new Router({
     {
       path: '/',
       name: 'videoPage',
-      component: videoPage
+      children: [
+        video
+      ]
     }
   ]
 })
---
+++ src/router/video/index.js
@@ -0,0 +1,9 @@
+import videoPage from '@/views/video/index.vue'
+const video = {
+  path: 'video',
+  name: 'video',
+  component: videoPage,
+  children: [{
+  }]
+}
+export default video
